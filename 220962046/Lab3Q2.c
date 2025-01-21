d55a313e6d9bb54347ced7d45205d0b1a6c493c0
Amulya Parashar <120652394+KING-258@users.noreply.github.com>
2025-01-21 09:33:16 +0530

    PCAP

--- 220962046/Lab3Q2.c
+++ 220962046/Lab3Q2.c
@@ -1,29 +1,32 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <mpi.h>
 #include <stdlib.h>
-int main(int argc, char** argv) {
-    int r, s, n, m, *arr = NULL, local_sum = 0, total_sum = 0, total_avg = 0;
+int main(int argc, char **argv){
+    int rank, size, *ori, *store, m;
+    float avg = 0.0, ans;
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &r);
-    MPI_Comm_size(MPI_COMM_WORLD, &s);
-    if(r == 0){
-        scanf("%d %d", &n, &m);
-        arr = (int*)malloc(n * m * sizeof(int));
-        for(int i = 0; i < n * m; i++){
-            scanf("%d", &arr[i]);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (rank == 0){
+        printf("Enter Value of M : ");
+        scanf("%d", &m);
+        ori = (int *)malloc((m * size) * sizeof(int));
+        printf("Enter the elements : \n");
+        for(int i = 0; i < m * size; ++ i){
+            scanf("%d", &ori[i]);
         }
     }
     MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    int* local_arr = (int*)malloc(m * sizeof(int));
-    MPI_Scatter(arr, m, MPI_INT, local_arr, m, MPI_INT, 0, MPI_COMM_WORLD);
-    for(int i = 0; i < m; i++){
-        local_sum += local_arr[i];
+    store = (int *)malloc(m * sizeof(int));
+    MPI_Scatter(ori, m, MPI_INT, store, m, MPI_INT, 0, MPI_COMM_WORLD);
+    for(int i = 0; i < m; ++i){
+        avg += store[i];
     }
-    MPI_Reduce(&local_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-    if(r == 0){
-        total_avg = total_sum / s;
-        printf("%d\n", total_avg);
+    avg /= m;
+    printf("%f from rank %d\n", avg, rank);
+    MPI_Reduce(&avg, &ans, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
+    if(rank == 0){
+        printf("%f is the answer\n", ans);
     }
     MPI_Finalize();
-    return 0;
 }
