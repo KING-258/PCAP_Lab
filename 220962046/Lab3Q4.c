9372fc0f58eb410193b6dc217a7ea35554923d87
Amulya Parashar <120652394+KING-258@users.noreply.github.com>
2025-01-21 09:36:21 +0530

    PCAP

--- 220962046/Lab3Q4.c
+++ 220962046/Lab3Q4.c
@@ -1,41 +1,45 @@
-#include <mpi.h>
 #include <stdio.h>
+#include <mpi.h>
 #include <stdlib.h>
 #include <string.h>
-int main(int argc, char** argv) {
-    int r, s, len;
-    char *s1 = NULL, *s2 = NULL;
+int main(int argc, char **argv){
+    int rank, size, l_size;
+    char *s1, *s2, *local_s1, *local_s2, *ans;
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &r);
-    MPI_Comm_size(MPI_COMM_WORLD, &s);
-    if (r == 0) {
-        char input1[1000], input2[1000];
-        scanf("%s %s", input1, input2);
-        len = strlen(input1);
-        s1 = input1;
-        s2 = input2;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    if (rank == 0){
+        s1 = (char *)malloc(100 * sizeof(char));
+        s2 = (char *)malloc(100 * sizeof(char));
+        printf("Enter String 1 : ");
+        scanf("%s", s1);
+        printf("Enter String 2 : ");
+        scanf("%s", s2);
+        l_size = (strlen(s1) / size);
+        ans = (char *)malloc(((strlen(s1) * 2) + 1) * sizeof(char));
+        ans[strlen(s1) * 2] = '\0';
     }
-    MPI_Bcast(&len, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    int chunk = len / s;
-    char* local_s1 = (char*)malloc((chunk + 1) * sizeof(char));
-    char* local_s2 = (char*)malloc((chunk + 1) * sizeof(char));
-    char* local_result = (char*)malloc((chunk * 2 + 1) * sizeof(char));
-    MPI_Scatter(s1, chunk, MPI_CHAR, local_s1, chunk, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Scatter(s2, chunk, MPI_CHAR, local_s2, chunk, MPI_CHAR, 0, MPI_COMM_WORLD);
-    for (int i = 0; i < chunk; i++) {
-        local_result[2 * i] = local_s1[i];
-        local_result[2 * i + 1] = local_s2[i];
+    MPI_Bcast(&l_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    local_s1 = (char *)malloc(l_size * sizeof(char));
+    local_s2 = (char *)malloc(l_size * sizeof(char));
+    MPI_Scatter(s1, l_size, MPI_CHAR, local_s1, l_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Scatter(s2, l_size, MPI_CHAR, local_s2, l_size, MPI_CHAR, 0, MPI_COMM_WORLD);
+    char *store = (char *)malloc((l_size + l_size + 1) * sizeof(char));
+    int check = 1, i = 0, j = 0, k = 0;
+    while (i < l_size || j < l_size){
+        if (check){
+            store[k++] = local_s1[i++];
+            check = 0;
+        }
+        else{
+            store[k++] = local_s2[j++];
+            check = 1;
+        }
     }
-    local_result[chunk * 2] = '\0';
-    char* final_result = NULL;
-    if (r == 0) {
-        final_result = (char*)malloc((len * 2 + 1) * sizeof(char));
-    }
-    MPI_Gather(local_result, chunk * 2, MPI_CHAR, final_result, chunk * 2, MPI_CHAR, 0, MPI_COMM_WORLD);
-    if (r == 0) {
-        final_result[len * 2] = '\0';
-        printf("%s\n", final_result);
+    store[l_size * 2] = '\0';
+    MPI_Gather(store, l_size * 2, MPI_CHAR, ans, l_size * 2, MPI_CHAR, 0, MPI_COMM_WORLD);
+    if(rank == 0){
+        printf("Modified String = %s\n", ans);
     }
     MPI_Finalize();
-    return 0;
 }
